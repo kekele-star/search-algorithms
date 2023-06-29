e78223f3d5cb61adbebeb19358ac868ea977c7c4
Martha EK. Demanya <m.demanya@bsl.com.gh>
2023-06-29 09:08:05 +0000

    Added linear search code

--- linear_search.py
+++ linear_search.py
@@ -6,6 +6,15 @@ def linear_search(list, target):
             return i
         return None
     
+    
+def linear_search(list, target):
+    # returns the index position of the target if found, else returns None
+
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+        return None
+    
 def verify(index):
     if index is not None:
         print("Target found at index: ", index)
