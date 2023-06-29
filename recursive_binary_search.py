7923469e22fdd91f25c235947154e9af743067d0
just_kekele <esinamdemanya@gmail.com>
2023-06-29 12:28:41 +0000

    recursive binary search

---
+++ recursive_binary_search.py
@@ -0,0 +1,8 @@
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
+
+        if list[midpoint] == target
+            return True
